838a94a620d10ae87e26b946f97ac84f8224ec03
Rick Mohamed <rickmohaa@gmail.com>
2023-08-09 00:40:03 +0300

    adding tailwind

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
-import styles from '../styles/Home.module.css';
 import Cart from './Cart';
+import Link from 'next/link';
 
 const Main = () => {
-    let [itemList, setItemList] = useState([]);
-    let [cartList, setCartList] = useState([]);
-    let [preTax, setPreTax] = useState(0.00);
+    const [itemList, setItemList] = useState([]);
+    const [cartList, setCartList] = useState([]);
+    const [preTax, setPreTax] = useState(0.00);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         fetchTodos();
         const savedCartList = JSON.parse(localStorage.getItem("cartList")) || [];
         setCartList(savedCartList);
-        console.log(savedCartList);
     }, []);
 
     useEffect(() => {
@@ -39,7 +39,6 @@ const Main = () => {
             name: itemName,
             price: price,
         };
-
         setCartList(prevCartList => [...prevCartList, itemData]);
     }
 
@@ -51,37 +50,63 @@ const Main = () => {
         setPreTax(tempPreTax);
     }
 
-    const removeitem = (whichItem) => {
+    const removeItem = (whichItem) => {
         let tempCart = cartList.filter((item, index) => index !== whichItem);
         setCartList(tempCart);
     }
 
+    const handleSearch = (event) => {
+        setSearchQuery(event.target.value);
+    }
+
+    // Filter items based on the search query
+    const filteredItemList = itemList.filter(item =>
+        item.itemName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return (
         <>
-            <div className={styles.main}>
-            <div className={styles.menu}>
-                <h1 className={styles.menuh1}>POS</h1>
-                
-                    <ul className={styles.menulist}>
-                        <li className={styles.menuitem}>Items List</li>
-                        <li className={styles.menuitem}>Add Item</li>
+            <div className="min-h-screen w-screen flex justify-end color-text-a3a2a2 overflow-hidden bg-gradient-to-b from-[#00020a]">
+                <div className="w-[30rem] h-full bg-white text-white">
+                    <h1 className="py-8 pl-16 text-orangered text-3xl">POS</h1>
+                    <ul className="flex flex-col gap-4 px-4 py-8">
+                        <Link href="/itemsList" className="px-3 py-2 text-lg bg-[#333] rounded cursor-pointer hover:text-orangered">Items List</Link>
+                        <li className="px-3 py-2 text-lg bg-[#333] rounded cursor-pointer hover:text-orangered">Add Item</li>
                     </ul>
                 </div>
-                <div className={styles.items}>
-                    <input placeholder="Search" type="search" className={styles.input} id="" />
-                    <div className={styles.list}>
-                        {itemList.length > 0 ? itemList.map((item, i) => (
-                            <button className={styles.li} key={i}
-                                onClick={() => addItem(item.id, item.itemName, item.price)}>
+                <div className="flex flex-col ml-auto px-4 py-4 space-y-4">
+                    {/* Search input */}
+                    <input
+                        placeholder="Search"
+                        type="search"
+                        className="w-56 h-12 outline-[1.5px solid orangered] rounded-lg text-white bg-transparent text-lg pl-4"
+                        value={searchQuery}
+                        onChange={handleSearch}
+                    />
+                    <div className="flex flex-wrap space-x-2">
+                        {/* Render filtered item list */}
+                        {filteredItemList.length > 0 ? filteredItemList.map((item, i) => (
+                            <button
+                                className="text-left flex items-center gap-2 rounded-lg w-32 h-32 bg-[#222] text-white text-lg py-4 px-8 hover:bg-[#313131]"
+                                key={i}
+                                onClick={() => addItem(item.id, item.itemName, item.price)}
+                            >
                                 {item.itemName + " - $" + item.price}
                             </button>
-                        )) : null}
+                        )) : (
+                            <p className="text-white">No items found.</p>
+                        )}
                     </div>
                 </div>
-                <Cart preTax={preTax} cartList={cartList} removeitem={removeitem} />
+                <Cart preTax={preTax} cartList={cartList} removeitem={removeItem} />
             </div>
         </>
     );
 }
 
 export default Main;
+
+
+
+
+
